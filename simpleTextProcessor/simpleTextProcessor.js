6d075f6ed05131ad15baf4c174a4a31119b0f94e
Maciej Śledziński <maciejs.sledzinski@cyfroweszkoly.pl>
2024-11-13 14:55:46 +0100

    commit testowy dwa

--- simpleTextProcessor/simpleTextProcessor.js
+++ simpleTextProcessor/simpleTextProcessor.js
@@ -1,1 +1,5 @@
-gggg
+const processText = (text, { uppercase = false, reverse = false, removeSpecialChars = false } = {}) =>{
+
+    const {text = "domyślny", uppercase = false, reverse = false, removeSpecialChars = false} = processText
+   
+} 
